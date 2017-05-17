0e55c9d71ef8b7890251ce377c58c965c1a2e97f
Bobby McDonald <bmcdonald@covermymeds.com>
2017-05-17 13:17:33 -0400

    Completed Rap Name Generator

--- js/script.js
+++ js/script.js
@@ -15,18 +15,71 @@
 function Generator() {
 
     /* Name Arrays: Customize names to change possible output */
-    this.last_names = ['the Chef', 'Digital', 'Wise', 'Knight', 'Wrecka', 'the Genius', 'the Zoo Keeper', 'the Monk', 'the Scientist', 'the Disciple', 'the Darkman', 'Pellegrino', 'the Ill Figure', 'Rocks The World', 'the Baptist',];
+    this.last_names = ['the Chef', 'Digital', 'Wise', 'Knight', 'Wrecka', 'the Genius', 'the Zoo Keeper', 'the Monk', 'the Scientist', 'the Disciple', 'the Darkman', 'Pellegrino', 'the Ill Figure', 'Rocks The World', 'the Baptist'];
     this.first_names = ['Inspectah', 'Masta', 'Poppa', 'Five Foot', 'Ghostface', 'Old Dirty'];
-
+    this.generateName = function(input){
+        //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#Getting_a_random_integer_between_two_values_inclusive
+        if(Math.floor(Math.random()*5) == 0){
+            input = input.toUpperCase().split('').join('.')
+        }
+        if (Math.floor(Math.random() * 2) == 0){
+            return this.first_names.random() + ' ' + input
+        }
+        else {
+            return input + ' ' + this.last_names.random()
+        }
+    }
 }
-
-
-//Add your codez here
-
 
 $(document).ready(function() {
 
     var engine = new Generator;
-    //Add your codez here
+    
+    
+    $('#enter').click(function(){
+        populateResponse(engine)
+    })
 
 });
+
+function populateResponse(generator){
+    var name = getName()
+    if (name != '') {
+        $('.response').text(generator.generateName(name))
+        displayResponse()
+    }
+    else{
+        hideResponse()
+    }
+}
+
+Array.prototype.random = function() {
+    return this[Math.floor(Math.random() * (this.length))]
+}
+
+function getName(){
+    if ($('#user-input').val() == '') {
+        displayError()
+        return ''
+    }
+    else {
+        hideError()
+        return $('#user-input').val()
+    }
+}
+
+function displayError(){
+    $('.error').css('display','block')
+}
+
+function hideError(){
+    $('.error').css('display','none')
+}
+
+function displayResponse(){
+    $('.response').css('display','block')
+}
+
+function hideResponse(){
+    $('.response').text('').css('display','none')
+}
